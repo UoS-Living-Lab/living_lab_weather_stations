6a2aa826c1fb9c4d685c5b4ff70d03688a78ce2b
Ethan Bellmer <e.bellmer@outlook.com>
2020-12-02 11:02:35 +0000

    Add selection for TTN activation mode

--- src/LoRaWAN.cpp
+++ src/LoRaWAN.cpp
@@ -5,18 +5,31 @@
 #include "init.h"
 #include "LoRaWAN.h"
 #include "ttn-otaa-credentials.h"
-//#include "ttn-apb-credentials.h"
+#include "ttn-apb-credentials.h"
 
+#define OTAA 1 // TODO: #2 OTAA not working with TTN Gateway
 
 void INIT_TTN()
 {
 		// Wait a maximum of 10s for Serial Monitor
-	while (!usbUSART && millis() < 10000)
-		;
+	//while (!usbUSART && millis() < 10000)
+	//	;
 
-	usbUSART.println("-- STATUS");
-	ttn.showStatus();
 
-	usbUSART.println("-- JOIN");
-	ttn.join(appEui, appKey);
+	if (OTAA)
+	{
+		usbUSART.println("-- STATUS");
+		ttn.showStatus();
+
+		usbUSART.println("-- JOIN");
+		ttn.join(appEui, appKey);
+	}
+	else
+	{
+		usbUSART.println("-- PERSONALIZE");
+		ttn.personalize(devAddr, nwkSKey, appSKey);
+
+		usbUSART.println("-- STATUS");
+		ttn.showStatus();
+	}
 }
