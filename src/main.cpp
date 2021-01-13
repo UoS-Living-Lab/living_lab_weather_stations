c7b43702a74cfda4a834b7433d26f0d971bf463b
Ethan Bellmer <e.bellmer@outlook.com>
2021-01-13 23:01:02 +0000

    Add RTC & fix sleep calls

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 #include "adc.h"
 #include "lowPower.h"
 
-
 // Variable Declarations
 // Function Declarations
 
@@ -33,27 +32,31 @@ void setup()
 	//INIT_TTN(); // Initialise the LoRa radio & connect to network // Disable for testing outside of LoRa range
 
 	I2C_INIT(); // Initialise the I2C interface
-	RTC_INIT(00,00,13,02,01,12,2020); // Init the RTC module // TODO: #1 Get datetime from TTN
-	MCU_SET_INT(); // Set pin interrupt for waking from sleep
+
+	RTC_INIT(00,05,10,05,01,01,21); // Init the RTC module // TODO: #1 Get datetime from TTN
+	
 	BME_INIT(); // Init the BME sensor on the I2C bus and set the sensor to low power sleep
 	SENSOR_ADC_INIT(); // Init the sensor ADC 
 	
-
-	RTC_SET_TIMER(1);
-
-	//uint8_t *payload = (uint8_t *) 200; // TTN test payload
-	
-
 	pinMode(PIN_PD2, INPUT);
 
+	RTC_DISABLE_TIMER_INTERRUPT(); // Disable timer interrupt in case it is active.
+	RTC_DISABLE_ALARM_1(); // Disable Alarm 1 interrupt in case it is active.
+	RTC_ENABLE_ALARM_2(); // Enable Alarm 2 interrupts
+	RTC_SET_ALARM_2();
+	RTC_CLEAR_STATUS();
+	//uint8_t *payload = (uint8_t *) 200; // TTN test payload	
+
+	MCU_ATTACH_INTERRUPT();	// Set pin interrupt for waking from sleep
+	
 
 	//ttn.sendBytes(payload, sizeof(payload));
-	delay(2000);
+	//delay(2000);
 }
 
 void loop()
 {
-	ledBlink();
+	//ledBlink();
 
 	BME_READ(); // Read data from the BME and return to sleep
 	
@@ -67,70 +70,43 @@ void loop()
 	float rp = BME_GET_REF_PRESSURE();
 	float dp = BME_GET_DEWPOINT_C();
 
-	/*
-	usbUSART.print(" Humidity: ");
-	usbUSART.print(h, 0);
-	
+	// BME280 Testing
+	usbUSART.print("Humidity: ");
+	usbUSART.print(h, 0);	
 	usbUSART.print(" Pressure: ");
 	usbUSART.print(p, 0);
-	
 	usbUSART.print(" Alt: ");
 	usbUSART.print(a, 1);
-	
 	usbUSART.print(" Temp: ");
 	usbUSART.print(t, 2);
-
 	usbUSART.print(" Dew Point: ");
 	usbUSART.print(dp, 2);
-
-	usbUSART.println("");
-	
 	usbUSART.print(" Reference pressure: ");
-	usbUSART.print(rp, 0);
+	usbUSART.println(rp, 0);
 	usbUSART.println("");
-	*/
 
 	float rainDetect = SENSOR_ADC_GET_CHANNEL(0);
 	float ch3 = SENSOR_ADC_GET_CHANNEL(3);
 	float ch1 = SENSOR_ADC_GET_CHANNEL(1);
 	float ch2 = SENSOR_ADC_GET_CHANNEL(2);
 
-	usbUSART.println("");
-
+	// Sensor ADC Testing
+	usbUSART.println("Sensor ADC Testing: ");
 	usbUSART.print("Rain Detect (ch0): ");
 	usbUSART.print(rainDetect,5);
-
 	usbUSART.print(" CH1: ");
 	usbUSART.print(ch1,5);
-
 	usbUSART.print(" CH2: ");
 	usbUSART.print(ch2,5);
-
 	usbUSART.print(" CH3: ");
-	usbUSART.print(ch3,5);
-
+	usbUSART.println(ch3,5);
 	usbUSART.println("");
 
-
+	// RTC Testing
+	usbUSART.print("RTC Testing - ");
 	usbUSART.print("INT PIN: ");
-	uint8_t INT_PIN = digitalRead(PIN_PD2);
-	usbUSART.print(INT_PIN);
+	usbUSART.println(digitalRead(PIN_PD2));
 
-	usbUSART.print(" 0x00: ");
-	uint8_t tmp = readRegister(0x00);
-	usbUSART.print(tmp);
-
-	usbUSART.print(" 0x01: ");
-	tmp = readRegister(0x01);
-	usbUSART.print(tmp);
-	
-	usbUSART.print(" 0x03: ");
-	tmp = readRegister(0x03);
-	usbUSART.print(tmp);
-	usbUSART.println("");
-
-	//usbUSART.println("TXing");
-	//ttn.sendBytes(payload, sizeof(payload)); //one byte, blocking function
-
-	delay(5000);
+	MCU_SLEEP();
+	RTC_CLEAR_STATUS();
 }
