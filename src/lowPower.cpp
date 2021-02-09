e54fb7c7129dd3564f21340730707520f1e64f54
Ethan Bellmer <e.bellmer@outlook.com>
2021-02-09 14:12:05 +0000

    Avoid usart print when mcu going to sleep.

--- src/lowPower.cpp
+++ src/lowPower.cpp
@@ -24,19 +24,27 @@ void MCU_DETECH_INTERRUPT()
 
 void MCU_SLEEP()
 {
+	usbUSART.println("");
+	usbUSART.println("going to sleep ...");
+	usbUSART.println("");
+
+	delay(100);
+
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 	sleep_enable();
 	MCU_ATTACH_INTERRUPT();
-	usbUSART.println("going to sleep ...");
-	delay(50);
+
 	sleep_mode();
 	//sleep_cpu();
 
 	sleep_disable();
 	MCU_DETECH_INTERRUPT();
 
-	delay(2000);
+	delay(2000); // Wait for clocks to come back up. Usually important for hardware USART.
+
+	usbUSART.println("");
 	usbUSART.println("Waking up...");
+	usbUSART.println("");
 }
 
 void MCU_WAKE_INT()
