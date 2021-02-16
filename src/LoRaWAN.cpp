1f94c0195228af397e3f80d1c828ce79654a3516
Ethan Bellmer <e.bellmer@outlook.com>
2021-02-16 00:53:20 +0000

    Add new functions for activation

--- src/LoRaWAN.cpp
+++ src/LoRaWAN.cpp
@@ -1,5 +1,8 @@
 /*
-
+	Title: LoRaWAN.cpp
+	Description: 
+	Date: 09/11/2020
+	Author: Ethan Bellmer 
 */
 
 #include "init.h"
@@ -7,24 +10,62 @@
 #include "ttn-otaa-credentials.h"
 #include "ttn-apb-credentials.h"
 
-#define OTAA 0 // TODO: #2 OTAA not working with TTN Gateway
+#define USE_OTAA 1 // TODO: #2 OTAA not working with TTN Gateway
 
-void INIT_TTN()
+
+void message(const uint8_t *payload, size_t size, port_t port);
+
+
+void TTN_INIT()
 {
-	if (OTAA)
+	switch(USE_OTAA)	
 	{
-		usbUSART.println("-- STATUS");
-		ttn.showStatus();
+		case 0:
+			usbUSART.println("-- PERSONALIZE");
+			ttn.personalize(devAddr, nwkSKey, appSKey);
 
-		usbUSART.println("-- JOIN");
-		ttn.join(appEui, appKey);
-	}
-	else
-	{
-		usbUSART.println("-- PERSONALIZE");
-		ttn.personalize(devAddr, nwkSKey, appSKey);
+			usbUSART.println("-- STATUS");
+			ttn.showStatus();
+		case 1:
+			ttn.onMessage(message);
+			usbUSART.println("-- JOIN");
+			ttn.join(appEui, appKey);
 
-		usbUSART.println("-- STATUS");
-		ttn.showStatus();
+			usbUSART.println("-- STATUS");
+			ttn.showStatus();
 	}
+}
+
+void TTN_SHOW_STATUS()
+{
+	ttn.showStatus();
+}
+
+size_t GET_TTN_HE_EUI()
+{
+	char *buffer[512];
+	return ttn.getHardwareEui(*buffer, sizeof(buffer));
+}
+
+void TTN_SEND_BYTES(uint8_t payload[], port_t port = 1)
+{
+	ttn.sendBytes(payload, sizeof(payload)); // Send payload data to TTN
+}
+
+int8_t TTN_GET_BYTES(port_t port = 1)
+{
+	return ttn.poll(port);
+}
+
+void message(const uint8_t *payload, size_t size, port_t port)
+{
+  usbUSART.println("-- MESSAGE");
+  usbUSART.print("Received " + String(size) + " bytes on port " + String(port) + ":");
+
+  for (int i = 0; i < size; i++)
+  {
+    usbUSART.print(" " + String(payload[i]));
+  }
+
+  usbUSART.println();
 }
